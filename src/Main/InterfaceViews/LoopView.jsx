328680961d646b61590f5478202125f152b52f46
ricardo.emanuel <ricardo.emanuel@inesctec.pt>
2024-09-09 10:33:06 +0100

    Uncommented some hardcoded examples.

--- src/Main/InterfaceViews/LoopView.jsx
+++ src/Main/InterfaceViews/LoopView.jsx
@@ -24,12 +24,10 @@ function LoopView() {
     mmr_divisor: 2,
   });
 
-  //TODO Update here to the commented code to use non-testing data
   useEffect(() => {
     setFormData((prev) => ({
       ...prev,
-      //meter_ids: meters
-      meter_ids: ["Meter#1", "Meter#2"],
+      meter_ids: meters
     }));
   }, [meters]);
   useEffect(
